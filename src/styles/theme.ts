f4dc2d831e02e931ed758dbdb7d8e89b2c81e3fb
Vinicius-SO <vhisoares39@gmail.com>
2022-07-21 22:04:20 -0300

    chakra config

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+    colors:{
+        gray:{
+            "900": '#181823',
+            "800": '#1F2829',
+            "700": '#353646',
+            "600": '#4B4D63',
+            "500": '#616488',
+            "400": '#79709A',
+            "300": '#9699BB',
+            "200": '#B3B5C6',
+            "100": '#D1D2DC',
+            "50": '#EEEEF2'
+        }
+    },
+    styles: {
+        global: {
+            body: {
+              bg: 'gray.900',
+              color: 'gray.50'
+            }
+        }
+    }
+
+})
